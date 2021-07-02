136dfbd5c43f5e0d52e804de3e633c5f797dffa6
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-07-02 18:38:27 +0800

    remove ids

--- services/config.js
+++ services/config.js
@@ -6,7 +6,7 @@
       "host" : "api.sandbox.paypal.com",
       "port" : "",
       "client_id" : "AdctUzplqNcGtujCgCTU3LjCO8grdgAZm7C0IPEYMa5B8yjqvlb-f6zbwl3CZXLM9m_76TMe2Qq78XiZ",  // your paypal serverlication client id
-      "client_secret" : "EGtT_MkVpMYkyTKIheab6VofYG_Izk-Mzs6telG0ZqQQQxOU65vCcPuPT2BnbAYxvoEn1oWo1TtTerw6" // your paypal serverlication secret id
+      "client_secret" : "" // your paypal serverlication secret id
     }
     paypal.configure(config);
   };
